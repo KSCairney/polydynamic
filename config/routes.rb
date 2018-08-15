8ae9380d42adad0f3a18d14017e74956cfad1905
Kenneth Cairney <kscairney@gmail.com>
2018-08-15 21:24:48 +0000

    added nav collapse

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'simple_pages/thank_you'
+
+  get 'simple_pages/about'
+
+  get 'simple_pages/contact'
+
   get 'simple_pages/index'
   
   root 'simple_pages#index'
