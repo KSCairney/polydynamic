2a4f87d8afc7982363986f085888d7e23f27b519
Kenneth Cairney <kscairney@gmail.com>
2018-08-17 22:58:12 +0000

    contact page work

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,4 +1,16 @@
 class SimplePagesController < ApplicationController
   def index
   end
+  
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @phone = params[:phone]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'iemjaxemail@example.com',
+        subject: "A new contact form message from #{@name} #{@email} #{@phone}",
+        body: @message).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'simple_pages/index'
   
+  post 'simple_pages/thank_you'
+  
   root 'simple_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
